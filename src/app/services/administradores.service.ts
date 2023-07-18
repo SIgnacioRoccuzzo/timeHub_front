daddb540ed46b6aeaf0919ec0f2bbbb296e9e340
Lidia <lidiayang104@gmail.com>
2023-07-18 18:09:26 +0200

    parte de admin y menu hecha

--- src/app/services/administradores.service.ts
+++ src/app/services/administradores.service.ts
@@ -1,9 +1,39 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
+import { Administrador } from '../interfaces/administrador.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdministradoresService {
 
-  constructor() { }
+  private httpServicio = inject(HttpClient)
+  private baseUrl: string;
+
+  constructor() {
+    this.baseUrl = 'http://localhost:3000/api/admins'
+  }
+
+  getRegistro(contenido: any): Promise<Administrador> {
+    return firstValueFrom(
+      this.httpServicio.post<Administrador>(`${this.baseUrl}/registro`, contenido)
+    )
+  }
+
+  getLogin(contenido: any): Promise<any> {
+    return firstValueFrom(
+      this.httpServicio.post<any>(`${this.baseUrl}/login`, contenido)
+    )
+  }
+
+  // getAllAdmin(): Promise<Administrador[]>{
+  //   return firstValueFrom(
+  //     this.httpServicio.get<Administrador[]>(this.baseUrl)
+  //   )
+  // }
+
+  isLogged(): boolean {
+    return localStorage.getItem('admin_token') ? true : false
+  }
 }
