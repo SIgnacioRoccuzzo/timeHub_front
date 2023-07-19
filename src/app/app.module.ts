bf749b04f460e2d9ab869bc078b6dfcf2861f759
vanessa <vanessaduquerubio@gmail.com>
2023-07-19 11:09:30 +0200

    creacion componentes perfil

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,9 @@ import { MenuComponent } from './components/menu/menu.component';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AuthInterceptor } from './interceptors/auth.interceptor';
+import { DatosPerfilComponent } from './pages/usuarios/Perfil/components/datos-perfil/datos-perfil.component';
+import { ProyectosComponent } from './pages/usuarios/Perfil/components/proyectos/proyectos.component';
+import { HorasComponent } from './pages/usuarios/Perfil/components/horas/horas.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +26,10 @@ import { AuthInterceptor } from './interceptors/auth.interceptor';
     PerfilComponent,
     RegistroComponent,
     LoginComponent,
-    MenuComponent
+    MenuComponent,
+    DatosPerfilComponent,
+    ProyectosComponent,
+    HorasComponent
   ],
   imports: [
     BrowserModule,
