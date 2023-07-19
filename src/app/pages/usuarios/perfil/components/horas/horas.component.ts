33f0be711765d0bd69f2dd9dac4592fede7dfe2b
SIgnacioRoccuzzo <129063696+SIgnacioRoccuzzo@users.noreply.github.com>
2023-07-19 18:39:05 +0200

    correcion

--- src/app/pages/usuarios/perfil/components/horas/horas.component.ts
+++ src/app/pages/usuarios/perfil/components/horas/horas.component.ts
@@ -1,11 +1,8 @@
-<<<<<<< HEAD
 import { Component, ViewChild } from '@angular/core';
 import { ChartConfiguration, ChartData, ChartType } from 'chart.js';
 import { BaseChartDirective } from 'ng2-charts';
 import { inject } from '@angular/core';
-=======
-import { Component, inject } from '@angular/core';
->>>>>>> 05f0322167ebf95668d62047993581b89376a8f1
+
 import { ActivatedRoute } from '@angular/router';
 import { UsuariosService } from 'src/app/services/usuarios.service';
 
@@ -85,27 +82,6 @@ export class HorasComponent {
 
   }
 
-  //Services
-  usuariosService = inject(UsuariosService);
-  activatedRoute = inject(ActivatedRoute)
-
-  timerActive: boolean = false;
-  registros: any[] = [];
-  horas_dedicadas: number = 0;
-
-
-  toggleTimer($event: any) {
-    this.horas_dedicadas = $event.target.value;
-    this.activatedRoute.params.subscribe(async params => {
-      const horasTrabajadas = await this.usuariosService.getbyDate(params['idUsuario'], this.horas_dedicadas);
-      if (horasTrabajadas && horasTrabajadas.length > 0) {
-        this.registros = horasTrabajadas[0].horas_dedicadas;
-      } else {
-        console.log('No se encontraron horas trabajadas.');
-      }
-    });
-
-  }
 
 }
 
