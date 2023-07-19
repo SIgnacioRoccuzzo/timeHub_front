59eb4333bb6241aaee6fb600fb5a2cdc1a73a6e0
vanessa <vanessaduquerubio@gmail.com>
2023-07-19 17:52:25 +0200

    maquetacion parte 1 perfil

--- src/app/pages/usuarios/perfil/components/horas/horas.component.ts
+++ src/app/pages/usuarios/perfil/components/horas/horas.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { ChartConfiguration, ChartData, ChartType } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
+
+
 
 @Component({
   selector: 'app-horas',
@@ -6,5 +10,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./horas.component.css']
 })
 export class HorasComponent {
+  @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
+  public barChartOptions: ChartConfiguration['options'] = {
+    responsive: true,
+    // We use these empty structures as placeholders for dynamic theming.
+    scales: {
+      x: {},
+      y: {
+        min: 10
+      }
+    },
+    plugins: {
+      legend: {
+        display: true,
+      }
+    }
+  };
+  public barChartType: ChartType = 'bar';
+  public barChartData: ChartData<'bar'> = {
+    labels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
+    datasets: [
+      { data: [50, 50, 50, 51, 50, 50, 50], label: 'Proyecto 1' },
+      { data: [28, 48, 40, 19, 86, 27, 90], label: 'Proyecto 2' }
+    ]
+  };
+  public chartClicked({ event, active }: { event: MouseEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
+
+  public chartHovered({ event, active }: { event: MouseEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
+
+  public randomize(): void {
+    // Only Change 3 values
+    this.barChartData.datasets[0].data = [
+      Math.round(Math.random() * 100),
+      59,
+      80,
+      Math.round(Math.random() * 100),
+      56,
+      Math.round(Math.random() * 100),
+      40];
+
+    this.chart?.update();
+  }
 
 }
+
+
+
+
--- src/app/pages/usuarios/perfil/perfil.component.ts
+++ src/app/pages/usuarios/perfil/perfil.component.ts
@@ -1,9 +1,6 @@
 
-import { Component, inject } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Proyecto } from 'src/app/interfaces/proyecto.interface';
-import { Usuario } from 'src/app/interfaces/usuario.interface';
-import { UsuariosService } from 'src/app/services/usuarios.service';
+import { Component } from '@angular/core';
+
 
 
 @Component({
@@ -15,15 +12,5 @@ export class PerfilComponent {
 
 
 }
-
-
-
-
-/* async cambioHora() {
-  const fecha = '2023-07-17'
-  this.activatedRoute.params.subscribe(async params =>
-    this.proyecto = await this.usuarioServices.getProyectos(params['idUsuario'], fecha))
-  console.log('proyecto', this.proyecto)
-*/
 
 
