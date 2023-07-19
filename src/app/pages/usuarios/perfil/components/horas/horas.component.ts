05f0322167ebf95668d62047993581b89376a8f1
SIgnacioRoccuzzo <129063696+SIgnacioRoccuzzo@users.noreply.github.com>
2023-07-19 17:52:49 +0200

    fx en usuario service

--- src/app/pages/usuarios/perfil/components/horas/horas.component.ts
+++ src/app/pages/usuarios/perfil/components/horas/horas.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { UsuariosService } from 'src/app/services/usuarios.service';
+
+
 
 @Component({
   selector: 'app-horas',
@@ -7,4 +11,40 @@ import { Component } from '@angular/core';
 })
 export class HorasComponent {
 
+  //Services
+  usuariosService = inject(UsuariosService);
+  activatedRoute = inject(ActivatedRoute)
+
+  timerActive: boolean = false;
+  registros: any[] = [];
+  horas_dedicadas: number = 0;
+
+
+  toggleTimer($event: any) {
+    this.horas_dedicadas = $event.target.value;
+    this.activatedRoute.params.subscribe(async params => {
+      const horasTrabajadas = await this.usuariosService.getbyDate(params['idUsuario'], this.horas_dedicadas);
+      if (horasTrabajadas && horasTrabajadas.length > 0) {
+        this.registros = horasTrabajadas[0].horas_dedicadas;
+      } else {
+        console.log('No se encontraron horas trabajadas.');
+      }
+    });
+
+  }
+
 }
+
+
+/*toggleTimer($event: any) {
+  this.horas_dedicadas = $event.target.value
+  this.activatedRoute.params.subscribe(async params => {
+    const horasTrabajadas = await this.usuariosService.getbyDate(params['idUsuario'], this.horas_dedicadas);
+    this.registros = horasTrabajadas[0].horas_dedicadas
+
+  })
+  */
+
+
+
+
